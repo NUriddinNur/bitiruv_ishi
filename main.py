d6f65ed483a17b91a6664d6b77d3457c3380cac0
Akbarali <girtbezbet@gmail.com>
2021-10-11 17:14:49 +0500

    first message

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("salom       assalom")
+
+
+
+class User:
+    def __init__(self):
+        self.name = None
+        self.nic_name = None
+        self.age = None
+        self.login = None
+        self.password = None
+        self.singl = True
+
+   #__________________________________________________ main function
+    def registr_or_login(self):
+        pass
+
+
+
+
+    # ______________________________________________ message function ______________________________________
+    def first_message(self):
+        print("""
+            register    [1]
+            login       [2]
+            
+        """)
+
+
+
+
