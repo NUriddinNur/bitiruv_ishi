08c2a8b09bf03178c01b734f804aba7f640b1b63
NuRiDDiN <Nurik900707@gmail.com>
2021-10-11 16:07:48 +0500

    first message

---
+++ bitiruv_ishi.py
@@ -0,0 +1,36 @@
+
+
+
+class User:
+    def __init__(self):
+        self.name = None
+        self.nic_name = None
+        self.age = None
+        self.login = None
+        self.password = None
+        self.single = True
+        self.user_id = None
+
+    # __________________________________________ main function ___________________________________________________
+    def regist_or_login(self):
+    	print()
+        pass
+
+
+
+
+    # ________________________________________ message function ___________________________________________________
+    @staticmethod
+    def first_message():
+        print("""
+            register    [1]
+            login       [2]
+        """)
+
+
+
+
+
+
+
+
